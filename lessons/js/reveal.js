b8d5da5a76900a6546dd2da59cbe1cc94d9aa29e
Shauna <shaunagm@gmail.com>
2014-07-09 18:22:17 -0400

    Adds self-guided version

--- lessons/js/reveal.js
+++ lessons/js/reveal.js
@@ -19,7 +19,7 @@ var Reveal = (function(){
 
 			// The "normal" size of the presentation, aspect ratio will be preserved
 			// when the presentation is scaled to fit different resolutions
-			width: 960,
+			width: 1150, // Also changed in the min version - SGM
 			height: 700,
 
 			// Factor of the display size that should remain empty around the content
